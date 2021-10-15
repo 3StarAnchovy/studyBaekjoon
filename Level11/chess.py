a985e71bd29f0c59550428af3635f212cc2445f7
Hong Ji Min <hongjimin@Hongui-MacBookPro.local>
2021-10-15 19:18:34 +0900

    fixChess

--- Level11/chess.py
+++ Level11/chess.py
@@ -1,23 +1,32 @@
 
-def inputXY():
-    y, x = map(int, input().split())
-    ChessMap = []
-    for i in range(y):
-        ChessMap.append(list(input()))
-    print(ChessMap)
-
-
 def drawMap(ChessMap, x, y):
     cmp1 = "BWBWBWBW"
     cmp2 = "WBWBWBWB"
+    map1 = [cmp1, cmp2, cmp1, cmp2, cmp1, cmp2, cmp1, cmp2]  # black이 먼저인 체스판
+    map2 = [cmp2, cmp1, cmp2, cmp1, cmp2, cmp1, cmp2, cmp1]  # white가 먼저인 체스판
     min = 0
-    cnt = 0
-    
-    for i_y in range(y - 8): # 8 x 8
-        for i_x in range(x - 8):
-            
+
+    for i_y in range(y - 8 + 1):  # 8 x 8 # map 탐색
+        for i_x in range(x - 8 + 1):
+            cnt = 0
+            for j_y in range(8):
+                for j_x in range(8):
+                    if(ChessMap[i_y + j_y][i_x + j_x] != map1[j_y][j_x]):
+                        cnt += 1
+            if (min < cnt):
+                min = cnt
+            cnt = 0
+            for j_y in range(8):
+                for j_x in range(8):
+                    if(ChessMap[i_y + j_y][i_x + j_x] != map2[j_y][j_x]):
+                        cnt += 1
+            if (min < cnt):
+                min = cnt
     return (min)
 
 
-inputXY()
-drawMap(ChessMap, x, y)
+y, x = map(int, input().split())
+ChessMap = []
+for i in range(y):
+    ChessMap.append(list(input()))
+print(drawMap(ChessMap, x, y))
