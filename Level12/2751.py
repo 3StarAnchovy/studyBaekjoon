a37905dfe088e50dfed6b59ba0dfe765fdb6d82c
Hong Ji Min <hongjimin@Hongui-MacBookPro.local>
2021-10-24 20:55:59 +0900

    solveHeapSoart

--- Level12/2751.py
+++ Level12/2751.py
@@ -1,21 +1,40 @@
-def heapSort(arr, num):
-    # Min heap 생성
-    for i in range(1, num):
-        c = i
-        while (1):
-            rootIndex = (c - 1) // 2
-            if (arr[rootIndex] < arr[c]):
-                temp = arr[rootIndex]
-                arr[rootIndex] = arr[c]
-                arr[c] = temp
-            c = rootIndex
-            if (c == 0):
-                break
-    print(arr)
+def swapArr(arr, rootIndex, childIndex):
+    temp = arr[rootIndex]
+    arr[rootIndex] = arr[childIndex]
+    arr[childIndex] = temp
 
+
+def heapfify(arr, num):
+    for i in range(1, num):
+        childIndex = i
+        while(1):
+            rootIndex = (childIndex - 1) // 2
+            if(arr[rootIndex] < arr[childIndex]):
+                swapArr(arr, rootIndex, childIndex)
+            childIndex = rootIndex
+            if (childIndex == 0):
+                break
+
+
+def heapSort(arr, num):
+    for i in range(num - 1, -1, -1):
+        swapArr(arr, 0, i)
+        rootIndex = 0
+        childIndex = 1
+        while(childIndex < i):
+            childIndex = 2 * rootIndex + 1
+            if (childIndex + 1 < i and arr[childIndex] < arr[childIndex + 1] ):
+                childIndex += 1
+            if (childIndex < i and arr[rootIndex] < arr[childIndex]):
+                swapArr(arr, rootIndex, childIndex)
+            rootIndex = childIndex
+    
 
 num = int(input())
 arr = []
 for i in range(num):
     arr.append(int(input()))
+heapfify(arr, num)
 heapSort(arr, num)
+for i in arr:
+    print(i)
--- Level12/heapSort.py
+++ Level12/heapSort.py
@@ -1,23 +1,40 @@
-def swapArray(a,b,arr):
-    temp = arr[a]
-    arr[a] = arr[b]
-    arr[b] = temp
+def swapArr(arr, rootIndex, childIndex):
+    temp = arr[rootIndex]
+    arr[rootIndex] = arr[childIndex]
+    arr[childIndex] = temp
 
-def heapfify(num, arr):
+
+def heapfify(arr, num):
     for i in range(1, num):
         childIndex = i
         while(1):
             rootIndex = (childIndex - 1) // 2
             if(arr[rootIndex] < arr[childIndex]):
-                swapArray(rootIndex,childIndex,arr)
+                swapArr(arr, rootIndex, childIndex)
             childIndex = rootIndex
             if (childIndex == 0):
                 break
-    print(arr)
 
+
+def heapSort(arr, num):
+    for i in range(num - 1, -1, -1):
+        swapArr(arr, 0, i)
+        rootIndex = 0
+        childIndex = 1
+        while(childIndex < i):
+            childIndex = 2 * rootIndex + 1
+            if (childIndex + 1 < i and arr[childIndex] < arr[childIndex + 1] ):
+                childIndex += 1
+            if (childIndex < i and arr[rootIndex] < arr[childIndex]):
+                swapArr(arr, rootIndex, childIndex)
+            rootIndex = childIndex
+    
 
 num = int(input())
 arr = []
 for i in range(num):
     arr.append(int(input()))
-heapfify(num, arr)
+heapfify(arr, num)
+heapSort(arr, num)
+for i in arr:
+    print(i)
