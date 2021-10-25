3c22299a7e24b4bb165a11ff0b02dddf78e33661
Hong Ji Min <hongjimin@Hongui-MacBookPro.local>
2021-10-25 19:45:38 +0900

    fin

--- Level12/heapSort.py
+++ Level12/heapSort.py
@@ -3,38 +3,37 @@ def swapArr(arr, rootIndex, childIndex):
     arr[rootIndex] = arr[childIndex]
     arr[childIndex] = temp
 
-
-def heapfify(arr, num):
+#MaxHeap 생성
+def heapify(num, arr):
     for i in range(1, num):
         childIndex = i
-        while(1):
+        rootIndex = 1
+        while(rootIndex != 0):
             rootIndex = (childIndex - 1) // 2
             if(arr[rootIndex] < arr[childIndex]):
                 swapArr(arr, rootIndex, childIndex)
             childIndex = rootIndex
-            if (childIndex == 0):
-                break
 
-
-def heapSort(arr, num):
+#크기를 한개씩 줄여가며 정렬
+#Heap 구조이므로 맨 앞에 값은 최대값임 -> 최대값을 맨 뒤로 보냄
+#난 천재야 역시
+def heapSort(num, arr):
     for i in range(num - 1, -1, -1):
-        swapArr(arr, 0, i)
+        swapArr(arr,0,i)
         rootIndex = 0
         childIndex = 1
-        while(childIndex < i):
-            childIndex = 2 * rootIndex + 1
-            if (childIndex + 1 < i and arr[childIndex] < arr[childIndex + 1] ):
+        while (childIndex < i):
+            childIndex = rootIndex * 2 + 1
+            if(childIndex + 1 < i and arr[childIndex] < arr[childIndex + 1]):
                 childIndex += 1
-            if (childIndex < i and arr[rootIndex] < arr[childIndex]):
-                swapArr(arr, rootIndex, childIndex)
+            if(childIndex < i and arr[childIndex] > arr[rootIndex]):
+                swapArr(arr,childIndex,rootIndex)
             rootIndex = childIndex
-    
+    print(arr)
 
 num = int(input())
 arr = []
 for i in range(num):
     arr.append(int(input()))
-heapfify(arr, num)
-heapSort(arr, num)
-for i in arr:
-    print(i)
+heapify(num, arr)
+heapSort(num, arr)
